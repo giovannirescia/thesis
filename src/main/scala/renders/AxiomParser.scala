12029b914b439b146fd0c135c41e37f40c0f9a68
Giovanni Rescia <giovannirescia@gmail.com>
2016-09-24 17:41:47 -0300

    Sacalaabailar

--- src/main/scala/renders/AxiomParser.scala
+++ src/main/scala/renders/AxiomParser.scala
@@ -1,4 +1,7 @@
 package renders
+import java.io.{File, FileWriter, PrintWriter, StringWriter}
+import javafx.print.Printer
+import java.io.FileOutputStream
 import org.semanticweb.owlapi.model.ClassExpressionType.{OBJECT_HAS_VALUE, OBJECT_INTERSECTION_OF, OBJECT_SOME_VALUES_FROM}
 import org.semanticweb.owlapi.model._
 import org.phenoscape.scowl._
@@ -8,63 +11,72 @@ import org.phenoscape.scowl._
 object AxiomParser {
   /**
     *
-    * @param axiom An OWL Equivalent Classes Axiom
+    * @param axioms An OWL Equivalent Classes Axioms List
+    * @param outFile A path to the new output file
+    *
     */
-  def snipp(axiom: OWLEquivalentClassesAxiom): Unit ={
-    val listExpr = axiom.getClassExpressionsAsList
-    val lfs = listExpr.get(0)
-    val rhs = listExpr.get(1)
+  def snipp(axioms: List[OWLEquivalentClassesAxiom], outFile: String): Unit ={
+    val writer = new PrintWriter(new FileOutputStream(new File(s"/Users/giovannirescia/coding/tesis/output/$outFile.txt"),true))
+    for (axiom <- axioms) {
+      val listExpr = axiom.getClassExpressionsAsList
+      val lfs = listExpr.get(0)
+      val rhs = listExpr.get(1)
 
-    if (!lfs.isAnonymous){
-      println(lfs.asOWLClass().getIRI.getShortForm)
-    }
-    println(" <- \u2263 ->")
-    if (!rhs.isAnonymous){
-      println(rhs.asOWLClass().getIRI.getShortForm)
-    }
-    else if (rhs.getClassExpressionType == OBJECT_INTERSECTION_OF){
-      val xs = matchIntersectionOF(rhs)
-      val xsLhs = xs(0)
-      val xsRhs = xs(1)
-      if (!xsLhs.isAnonymous){
-        println(xsLhs.asOWLClass().getIRI.getShortForm)
+      if (!lfs.isAnonymous) {
+        writer.write(lfs.asOWLClass().getIRI.getShortForm)
       }
-
-
-      if (!xsRhs.isAnonymous){
-        println(xsRhs.asOWLClass().getIRI.getShortForm)
+      writer.write(" \u2263 ")
+      if (!rhs.isAnonymous) {
+        writer.write(rhs.asOWLClass().getIRI.getShortForm)
       }
-      else{
-        for(x <- xs.tail){
-          println(" <- " + "\u2229" + " -> ")
+      else if (rhs.getClassExpressionType == OBJECT_INTERSECTION_OF) {
+        val xs = matchIntersectionOF(rhs)
+        val xsLhs = xs(0)
+        val xsRhs = xs(1)
+        if (!xsLhs.isAnonymous) {
+          writer.write(xsLhs.asOWLClass().getIRI.getShortForm)
+        }
 
-          if (x.getClassExpressionType == OBJECT_HAS_VALUE){
-            val ys = matchHasValue(x)
-            val ysLhs = ys._1
-            val ysRhs = ys._2
 
-            println(ysLhs.asOWLObjectProperty().getIRI.getShortForm)
+        if (!xsRhs.isAnonymous) {
+          writer.write(xsRhs.asOWLClass().getIRI.getShortForm)
+        }
+        else {
+          for (x <- xs.tail) {
+            writer.write(" \u2229 ")
 
-            println(" <-  Value:  -> ")
+            if (x.getClassExpressionType == OBJECT_HAS_VALUE) {
+              val ys = matchHasValue(x)
+              val ysLhs = ys._1
+              val ysRhs = ys._2
 
-            println(ysRhs.getIRI.getShortForm)
+              writer.write("(" + ysLhs.asOWLObjectProperty().getIRI.getShortForm)
 
-          }else if(x.getClassExpressionType == OBJECT_SOME_VALUES_FROM){
-            val ys = matchSomeValuesFrom(x)
-            val ysLhs = ys._1
-            val ysRhs = ys._2
+              writer.write(" value: ")
 
-            println(ysLhs.asOWLObjectProperty().getIRI.getShortForm)
+              writer.write(ysRhs.getIRI.getShortForm + ")")
 
-            println(" <- Some -> ")
+            } else if (x.getClassExpressionType == OBJECT_SOME_VALUES_FROM) {
+              val ys = matchSomeValuesFrom(x)
+              val ysLhs = ys._1
+              val ysRhs = ys._2
 
-            println(ysRhs.asOWLClass().getIRI.getShortForm)
+              writer.write("(" + ysLhs.asOWLObjectProperty().getIRI.getShortForm)
+
+              writer.write(" some ")
+
+              writer.write(ysRhs.asOWLClass().getIRI.getShortForm + ")")
+            }
+
           }
 
         }
 
       }
+     writer.write("\n")
+
     }
+    writer.close()
   }
 
   def matchSomeValuesFrom(given: OWLClassExpression): Tuple2[OWLObjectPropertyExpression, OWLClassExpression] = given match{
